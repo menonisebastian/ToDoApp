d1a1a37a5e71c2572bf4c412e65c4d750132af0a
menonisebastian <arturomenoni@gmail.com>
2025-10-30 21:03:51 +0100

    v2 - (new Colors, FontLogo, PillShaped tasks)

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -32,6 +34,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -39,6 +42,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -50,6 +54,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -61,6 +66,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.todoapp.ui.theme.ToDoAppTheme
+import com.example.todoapp.ui.theme.Typography
 
 @Composable
 fun AppNav()
@@ -131,7 +137,7 @@ fun Login(onEnviar: (String, String)-> Unit)
 {
     var nombres by remember { mutableStateOf("") }
     var alias by remember { mutableStateOf("") }
-    var checked by remember { mutableStateOf(false) }
+
     // 1. Add a state to control the dialog's visibility
     var showDialog by remember { mutableStateOf(false) }
 
@@ -146,25 +152,29 @@ fun Login(onEnviar: (String, String)-> Unit)
             .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Spacer(Modifier.height(20.dp))
         Image(
             painter = painterResource(R.drawable.applogo),
-            modifier = Modifier.size(100.dp).padding(20.dp),
-            contentDescription = "Imagen"
+            modifier = Modifier.size(100.dp).padding(vertical = 10.dp),
+            contentDescription = "Logo"
         )
-        Text(
-            text = "TO-DO APP",
-            fontSize = 30.sp,
-            color = Color(0xFF850071),
-            fontWeight = FontWeight.Bold,
-
-            )
+        Image(
+            painter = painterResource(R.drawable.fontlogo),
+            modifier = Modifier.width(200.dp).padding(top = 5.dp),
+            contentDescription = "logo texto"
+        )
+//        Text(
+//            text = "toDo(App)",
+//            fontSize = 30.sp,
+//            color = Color(0xFF505050),
+//            fontWeight = FontWeight.Bold
+//            )
         Spacer(Modifier.height(20.dp))
         Column(
             modifier = Modifier
                 .shadow(15.dp, RoundedCornerShape(10.dp))
                 .background(Color.White, RoundedCornerShape(10.dp))
                 .padding(20.dp)
-                .height(350.dp)
                 .width(300.dp),
             verticalArrangement = Arrangement.SpaceAround,
             horizontalAlignment = Alignment.CenterHorizontally
@@ -183,60 +193,57 @@ fun Login(onEnviar: (String, String)-> Unit)
                 singleLine = true,
                 label = { Text("Alias") }
             )
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
+//            Row(
+//                modifier = Modifier.fillMaxWidth(),
+//                verticalAlignment = Alignment.CenterVertically
+//
+//            ) {
+//                Checkbox(
+//                    checked = checked,
+//                    onCheckedChange = {checked = it}
+//                )
+//                Text(
+//                    text = "Mantener la sesion abierta"
+//                )
+//            }
+//
+//            Row(
+//                modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp),
+//                verticalAlignment = Alignment.CenterVertically
+//            ) {
+//                Text(
+//                    text = "Activar notificaciones"
+//                )
+//                Spacer(Modifier.weight(1f))
+//                Switch(
+//                    checked = checked,
+//                    onCheckedChange =
+//                    {
+//                        checked = it
+//                    }
+//                )
+//            }
 
-            ) {
-                Checkbox(
-                    checked = checked,
-                    onCheckedChange = {checked = it}
-                )
-                Text(
-                    text = "Mantener la sesion abierta"
-                )
-            }
+            Spacer(Modifier.height(10.dp))
 
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "Activar notificaciones"
-                )
-                Spacer(Modifier.weight(1f))
-                Switch(
-                    checked = checked,
-                    onCheckedChange =
-                    {
-                        checked = it
-                    }
-                )
-            }
-
-            HorizontalDivider(modifier = Modifier.padding(10.dp))
-
-            Button(onClick = {
+            ElevatedButton(onClick = {
                 if (nombres.isNotBlank() && alias.isNotBlank()) {
                     onEnviar(nombres, alias)
                 }
                 else
                 {
-                    showDialog = true
+//                  showDialog = true
+                    //no hace nada, se puede eliminar el else
                 }
             },
                 modifier = Modifier.width(275.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF850071)))
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFD6310)))
             {
                 Text(
-                    text = "Continuar"
+                    text = "Continuar",
+                    fontWeight = FontWeight.Bold
                 )
             }
-            Text(
-                text = "Gracias por registrarte",
-                fontSize = 16.sp,
-                color = Color(0xFF850071)
-            )
         }
     }
 }
@@ -252,81 +259,88 @@ fun App(nombre:String, alias:String, onBack: ()-> Unit)
         .fillMaxSize()
         .padding(20.dp), horizontalAlignment = Alignment.CenterHorizontally)
     {
-        Spacer(Modifier.height(15.dp))
+        Spacer(Modifier.height(30.dp))
 
-        // FILA NOMBRE Y PREFERENCIAS
-
-        Row (verticalAlignment = Alignment.CenterVertically)
+        Column(modifier = Modifier.shadow(15.dp, RoundedCornerShape(15.dp))
+            .background(Color.White, RoundedCornerShape(10.dp))
+            .padding(horizontal = 20.dp, vertical = 10.dp))
         {
-            Text("Hola, $nombre ($alias)", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-            Spacer(Modifier.weight(1f))
-            IconButton(onClick = {expanded = !expanded})
+            // FILA NOMBRE Y PREFERENCIAS
+
+            Row (verticalAlignment = Alignment.CenterVertically)
             {
-                Icon(Icons.Default.MoreVert, contentDescription = "Preferencias")
-                DropdownMenu(expanded = expanded,
-                    onDismissRequest = {expanded=false},
-                    modifier = Modifier.background(Color.White) )
+                Text("Hola, $nombre ($alias)", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = {expanded = !expanded})
                 {
-                    // First section
-                    DropdownMenuItem(
-                        text = { Text("Profile") },
-                        leadingIcon = { Icon(Icons.Outlined.Person, contentDescription = null) },
-                        onClick = { /* Do something... */ }
-                    )
-                    DropdownMenuItem(
-                        text = { Text("Settings") },
-                        leadingIcon = { Icon(Icons.Outlined.Settings, contentDescription = null) },
-                        onClick = { /* Do something... */ }
-                    )
+                    Icon(Icons.Default.MoreVert, contentDescription = "Preferencias")
+                    DropdownMenu(expanded = expanded,
+                        onDismissRequest = {expanded=false},
+                        modifier = Modifier.background(Color.White) )
+                    {
+                        // First section
+//                    DropdownMenuItem(
+//                        text = { Text("Profile") },
+//                        leadingIcon = { Icon(Icons.Outlined.Person, contentDescription = null) },
+//                        onClick = { /* Do something... */ }
+//                    )
+                        DropdownMenuItem(
+                            text = { Text("Preferencias") },
+                            leadingIcon = { Icon(Icons.Outlined.Settings, contentDescription = null) },
+                            onClick = { /* Do something... */ }
+                        )
 
-                    HorizontalDivider()
+                        HorizontalDivider()
 
-                    // Second section
-                    DropdownMenuItem(
-                        text = { Text("Send Feedback") },
-                        leadingIcon = { Icon(Icons.Outlined.Warning, contentDescription = null) },
-                        trailingIcon = { Icon(Icons.AutoMirrored.Outlined.Send, contentDescription = null) },
-                        onClick = { /* Do something... */ }
-                    )
+                        // Second section
+//                    DropdownMenuItem(
+//                        text = { Text("Send Feedback") },
+//                        leadingIcon = { Icon(Icons.Outlined.Warning, contentDescription = null) },
+//                        trailingIcon = { Icon(Icons.AutoMirrored.Outlined.Send, contentDescription = null) },
+//                        onClick = { /* Do something... */ }
+//                    )
 
-                    HorizontalDivider()
+                        HorizontalDivider()
 
-                    // Third section
-                    DropdownMenuItem(
-                        text = { Text("About") },
-                        leadingIcon = { Icon(Icons.Outlined.Info, contentDescription = null) },
-                        onClick = { /* Do something... */ }
-                    )
-                    DropdownMenuItem(
-                        text = { Text("Help") },
-                        // https://developer.android.com/develop/ui/compose/components/menu?hl=es-419
-                        //leadingIcon = { Icon(Icons.AutoMirrored.Outlined.Help, contentDescription = null) },
-                        //trailingIcon = { Icon(Icons.AutoMirrored.Outlined.OpenInNew, contentDescription = null) },
-                        onClick = { /* Do something... */ }
-                    )
+                        // Third section
+                        DropdownMenuItem(
+                            text = { Text("Ayuda") },
+                            leadingIcon = { Icon(Icons.Outlined.Info, contentDescription = null) },
+                            onClick = { /* Do something... */ }
+                        )
+//                    DropdownMenuItem(
+//                        text = { Text("Help") },
+//                        // https://developer.android.com/develop/ui/compose/components/menu?hl=es-419
+//                        leadingIcon = { Icon(Icons.AutoMirrored.Outlined.Help, contentDescription = null) },
+//                        trailingIcon = { Icon(Icons.AutoMirrored.Outlined.OpenInNew, contentDescription = null) },
+//                        onClick = { /* Do something... */ }
+//                    )
+                    }
                 }
             }
-        }
 
-        // FILA AGREGAR TAREA
+            // FILA AGREGAR TAREA
 
-        Row(modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp), verticalAlignment = Alignment.CenterVertically)
-        {
-            OutlinedTextField(
-                value = tarea,
-                onValueChange = { tarea = it },
-                label = { Text("Nueva tarea")}, modifier = Modifier.weight(1f)
-            )
-            Spacer(modifier = Modifier.width(10.dp))
-
-            Button(onClick = {
-                if (tarea.isNotBlank()) {
-                    tareas.add(tarea)   //agregar tarea a la lista de tareas
-                    tarea = ""          //limpiar el campo después de agregar
-                }
-            }, colors = ButtonDefaults.buttonColors(Color(0xFF850071)))
+            Row(modifier = Modifier.fillMaxWidth().padding(vertical = 10.dp), verticalAlignment = Alignment.CenterVertically)
             {
-                Text("Agregar")
+                OutlinedTextField(
+                    value = tarea,
+                    onValueChange = { tarea = it },
+                    label = { Text("Nueva tarea")}, modifier = Modifier.weight(1f),
+                    singleLine = true
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+
+                Button(onClick = {
+                    if (tarea.isNotBlank()) {
+                        tareas.add(tarea)   //agregar tarea a la lista de tareas
+                        tarea = ""          //limpiar el campo después de agregar
+                    }
+                }, colors = ButtonDefaults.buttonColors(Color(0xFFFD6310)))
+                {
+                    Text("Agregar",
+                        fontWeight = FontWeight.Bold)
+                }
             }
         }
 
@@ -334,18 +348,27 @@ fun App(nombre:String, alias:String, onBack: ()-> Unit)
 
         if (tareas.isEmpty())
         {
-            Text("Tu lista de tareas esta vacia", modifier = Modifier.padding(20.dp), fontStyle = FontStyle.Italic )
+            Column(modifier = Modifier.fillMaxHeight(),verticalArrangement = Arrangement.Center)
+            {
+                Text("Tu lista de tareas está vacía", modifier = Modifier.padding(20.dp),
+                    fontSize = 20.sp,
+                    fontStyle = FontStyle.Italic, color = Color.Gray)
+            }
         }
         else
         {
+            Spacer(Modifier.height(10.dp))
+            //HorizontalDivider()
             // Mostrar cada tarea en su propia fila
             tareas.forEachIndexed { index, tareaItem ->
                 Row(
-                    modifier = Modifier.fillMaxWidth().padding(10.dp),
+                    modifier = Modifier.shadow(15.dp, RoundedCornerShape(15.dp))
+                        .background(Color.White, RoundedCornerShape(10.dp))
+                        .fillMaxWidth()
+                        .padding(vertical = 10.dp, horizontal = 20.dp),
                     verticalAlignment = Alignment.CenterVertically
                 )
                 {
-
                     Text(text = tareaItem)
                     Spacer(modifier = Modifier.weight(1f))
 
@@ -353,19 +376,19 @@ fun App(nombre:String, alias:String, onBack: ()-> Unit)
                     {
                         Icon(Icons.Default.Delete, contentDescription = "Preferencias")
                     }
-
                 }
-                HorizontalDivider()
+                Spacer(Modifier.height(10.dp))
             }
         }
 
         Spacer(Modifier.weight(1f))
         Button(
             onClick = { onBack() },
-            colors = ButtonDefaults.buttonColors(Color(0xFF850071))
+            colors = ButtonDefaults.buttonColors(Color(0xFFFD6310))
         )
         {
-            Text(text = "Salir", color = Color.White)
+            Text(text = "Salir",
+                fontWeight = FontWeight.Bold)
         }
 
 //        FloatingActionButton(
@@ -380,7 +403,6 @@ fun App(nombre:String, alias:String, onBack: ()-> Unit)
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ToDoAppTheme {
-        AppNav()
-    }
+    AppNav()
+    //App(nombre = "Sebastian", alias = "Menoni", onBack = {})
 }
