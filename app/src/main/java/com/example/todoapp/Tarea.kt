04e84310906b3178ea5622fda3d7c47ff47b6d6e
menonisebastian <arturomenoni@gmail.com>
2025-12-02 16:19:05 +0100

    v20

--- app/src/main/java/com/example/todoapp/Tarea.kt
+++ app/src/main/java/com/example/todoapp/Tarea.kt
@@ -5,7 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "tareas")
 data class Tarea(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val texto: String,
     val fecha: String
 )
