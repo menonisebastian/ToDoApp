32be389407c6d83301dccbdd6d51187af96056b5
menonisebastian <arturomenoni@gmail.com>
2025-12-16 16:52:11 +0100

    v25 - Firebase

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -649,6 +649,7 @@ fun App(
     var showHelpDialog by remember { mutableStateOf(false) }
     var searchQuery by remember { mutableStateOf("") }
     var tareaDetallada by remember { mutableStateOf<Tarea?>(null) }
+    var expanded by remember { mutableStateOf(false) }
     val scope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
 
